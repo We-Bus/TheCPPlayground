86c2743905a03d5790c10a0d50bc361b6324b542
We-Bus <w.busscher@outlook.com>
2023-05-31 12:59:45 +0200

    added class

---
+++ project/src/BasicProgram/BasicProgram.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_BASICPROGRAM_H
+#define PROJECT_BASICPROGRAM_H
+
+
+class BasicProgram {
+
+};
+
+
+#endif //PROJECT_BASICPROGRAM_H
---
+++ project/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "BasicProgram/BasicProgram.h"
+
+int main() {
+    int i,n;
+    char * buffer;
+
+    printf("How Long do you want the string? ");
+    scanf("%d", &i);
+
+    buffer = (char*) malloc(i+1);
+    if (buffer==NULL) exit (1);
+
+    for(n=0; n<i; n++) {
+        buffer[n]=rand()%26+'a';
+    }
+    buffer[i]='\0';
+
+    printf("Random string: %s\n",buffer);
+    free(buffer);
+    return 0;
+}
